0e5b6a96ccc0afc91efbd3c71fba867c42937e18
moonspam <booboo0412@gmail.com>
2017-12-03 22:22:38 +0900

    add mobile layout

--- src/js/ui.js
+++ src/js/ui.js
@@ -11,12 +11,14 @@ var header = $("header");
 $(document).ready(function() {
   "use strict";
 
-  $.getJSON("//api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=e8337d945c60e268e0795d92f78d7fe3").done(function(data) {
+  var weatherApi = "//api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=e8337d945c60e268e0795d92f78d7fe3";
+
+  $.getJSON(weatherApi).done(function(data) {
     weather = data.weather["0"].icon;
     header.addClass("weather_" + weather);
   }).fail(function() {
     console.log("날씨 정보를 불러오지 못했습니다!");
-    header.addClass("weather_50d");
+    header.addClass("weather_01d");
   });
 
 });
