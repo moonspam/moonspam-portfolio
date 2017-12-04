97410aa770ae136f82bfd3588336fb8e07800d72
moonspam <booboo0412@gmail.com>
2017-12-04 21:06:02 +0900

    fixed something

--- src/js/ui.js
+++ src/js/ui.js
@@ -13,6 +13,12 @@ $(document).ready(function() {
 
   var weatherApi = "//api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=e8337d945c60e268e0795d92f78d7fe3";
 
+  $(".skill").find("li").each(function(index) {
+    var num = $(this).attr("class").replace("per_", "");
+    var per = num + "%";
+    $(this).attr("title", per);
+  });
+
   $.getJSON(weatherApi).done(function(data) {
     weather = data.weather["0"].icon;
     header.addClass("weather_" + weather);
